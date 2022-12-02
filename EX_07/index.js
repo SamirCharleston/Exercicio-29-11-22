942125a62275ca6b2a345b8b4887500d7b774562
SamirCharleston <samircharleston@gmail.com>
2022-12-02 00:29:21 -0300

    trabalho terminado

--- EX_07/index.js
+++ EX_07/index.js
@@ -1,5 +1,7 @@
 const formulario = document.getElementsByTagName("form")[0];
 const tabela = document.getElementById("tabela");
+const info = document.getElementsByClassName("info")[0];
+const atualizar = document.getElementById("atualizar");
 
 const campos = {
     nome: document.getElementById("nome"),
@@ -29,7 +31,7 @@ const dadosLista = [{
 lerDados();
 
 campos.adicionar.addEventListener("click", adicionarDados);
-
+atualizar.addEventListener("click", atualizarDados);
 
 function adicionarDados() {
     const regex = /[^a-z \s]/i;
@@ -43,21 +45,31 @@ function adicionarDados() {
     let validacaoNome = validacaoSalarioFilho = somenteLetras = false;
 
     dadosLidos.nome = campos.nome.value;
-    dadosLidos.filhos = Number(campos.filhos.value);
-    dadosLidos.salario = Number(campos.salario.value);
+    dadosLidos.filhos = campos.filhos.value;
+    dadosLidos.salario = campos.salario.value;
 
     somenteLetras = regex.test(dadosLidos.nome);
 
     if (dadosLidos.nome !== "" && !somenteLetras) {
         validacaoNome = true;
+
+        if (dadosLidos.filhos != "" && dadosLidos.salario != "") {
+
+            dadosLidos.filhos = Number(dadosLidos.filhos);
+            dadosLidos.salario = Number(dadosLidos.salario);
+
+            if (dadosLidos.filhos >= 0 && dadosLidos.salario >= 0) {
+                validacaoSalarioFilho = true;
+            } else {
+                alert("Preencha os campos Num. Filhos e Salário com valores válidos.");
+            }
+
+        } else {
+            alert("Preencha todos os campos.");
+        }
+
     } else {
         alert("Digite o nome, sem numeros ou caracteres especiais.");
-    }
-
-    if (dadosLidos.filhos >= 0 && dadosLidos.salario >= 0) {
-        validacaoSalarioFilho = true;
-    } else {
-        alert("Preencha os campos Num. Filhos e Salário com valores válidos.");
     }
 
     if (validacaoNome && validacaoSalarioFilho) {
@@ -70,10 +82,11 @@ function adicionarDados() {
                             <td>R$ ${dadosLidos.salario}</td>`
 
         dadosLista.push(dadosLidos);
+        atualizarDados();
+        formulario.reset();
     }
-
-    formulario.reset();
 }
+
 
 function lerDados() {
     const listaElementosTr = [];
@@ -89,4 +102,33 @@ function lerDados() {
                                             <td>${dadosLista[i].filhos}</td>
                                             <td>R$ ${dadosLista[i].salario}</td>`
     }
+}
+
+
+function atualizarDados() {
+    let mediaSalarial = quantidadePessoas = somaSalarios = mediaFilhos = somaFilhos = salarioMenor = maiorSalario = 0;
+
+    for (i = 0; i < dadosLista.length; i++) {
+        somaSalarios += Number(dadosLista[i].salario);
+        somaFilhos += Number(dadosLista[i].filhos);
+
+        if (maiorSalario < dadosLista[i].salario) {
+            maiorSalario = dadosLista[i].salario;
+        }
+
+        if (Number(dadosLista[i].salario) < 1500) {
+            salarioMenor++;
+        }
+    }
+
+    quantidadePessoas = Number(dadosLista.length);
+
+    mediaSalarial = somaSalarios / quantidadePessoas;
+    mediaFilhos = somaFilhos / quantidadePessoas;
+    percentualPessoas = salarioMenor / quantidadePessoas * 100;
+
+    info.innerHTML = `  <p>Media de salário da população: ${mediaSalarial}</p>
+                        <p>Media de número de filhos: ${mediaFilhos}</p>
+                        <p>Maior salario dos habitantes: ${maiorSalario}</p>
+                        <p>Percentual de pessoas com salário menor que R$ 1500,00: ${percentualPessoas}%</p>`;
 }
